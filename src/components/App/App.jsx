b910b344a8d52e01659f2173469ecc427333ed79
Ricesc <ricesc14@gmail.com>
2024-06-21 04:42:54 -0400

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Luke API Walker</h1>
+            <h2>En Desarrollo!</h2>
+        </div>
+    );
+}
+
+export default App;
